53bcbe6a0f9cf2a85663265e4e4a141cf0de25bd
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-10-20 18:31:59 +0300

    all Struct methods

--- init.rb
+++ init.rb
@@ -6,10 +6,34 @@ end
 # => Customer
 
 joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12345)
+john = Customer.new('John Smith', '123 Maple, Anytown NC', 12345)
 # => #<struct Customer name="Joe Smith", address="123 Maple, Anytown NC", zip=12345>
 
-puts joe.name       # => "Joe Smith"
-puts joe['name']    # => "Joe Smith"
-puts joe[:name]     # => "Joe Smith"
-puts joe[0]         # => "Joe Smith"
-puts joe.greeting   # => "Hello Joe Smith!"
+p joe.name       # => "Joe Smith"
+p joe['name']    # => "Joe Smith"
+p joe[:name]     # => "Joe Smith"
+p joe[0]         # => "Joe Smith"
+p joe.greeting   # => "Hello Joe Smith!"
+joe['name'] = 'John Smith'
+p joe[0]         # => "John Smith"
+joe.each {|x| p (x) }
+joe.each_pair {|name, value| p("#{name} => #{value}") }
+p joe.eql?(john)
+p joe.hash
+p john.hash
+p joe.length
+p joe.size
+p joe.members
+joe.select {|val| p val if val.is_a?(Fixnum)}
+p joe.to_a[1..2]
+p joe.values_at(1..2)
+p joe.values
+p joe.to_s
+p joe.inspect
+
+
+# https://ru.wikibooks.org/wiki/Ruby/%D0%A1%D0%BF%D1%80%D0%B0%D0%B2%D0%BE%D1%87%D0%BD%D0%B8%D0%BA/Struct
+# Методы класса
+# new
+# Методы объекта
+# []=, [], ==, each_pair, each, eql?, hash, inspect, length, members, select, size, to_a, to_s, values_at, values
