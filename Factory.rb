00f9dcb630ca971b259ee0034425cfbceb4f40d1
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-10-20 21:01:38 +0300

    add attr_accessor and initialize

--- Factory.rb
+++ Factory.rb
@@ -1,10 +1,20 @@
 class Factory
     def self.new(*args)
         p args
-        gen_class
+        gen_class(*args)
     end
 
-    def self.gen_class
-        Class.new
+    def self.gen_class(*args)
+        Class.new  do
+            attr_accessor(*args)
+            
+            define_method :initialize do |*ins_args|
+                #TODO ArgumentError Wrong number of argument (args.l !=  ins_args)
+                args.each_with_index do |arg,index|
+                    send("#{arg}=", ins_args[index])
+                end                
+            end
+
+        end
     end
 end
--- init.rb
+++ init.rb
@@ -1,35 +1,48 @@
+require_relative('./Factory')
+
 Customer = Struct.new(:name, :address, :zip) do 
   def greeting
     puts "Hello #{name}!"
   end
 end
-# => Customer
+
+FactoryClass = Factory.new(:name, :address, :zip)
+p FactoryClass.class
+p FactoryClass.superclass
+
+# FactoryClass = Factory.new(:name, :address, :zip) do 
+#   def greeting
+#     puts "Hello #{name}!"
+#   end
+# end
 
 joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12345)
-john = Customer.new('John Smith', '123 Maple, Anytown NC', 12345)
+john = FactoryClass.new('John Smith', '123 Maple, Anytown NC', 12345)
+p john.inspect
+p john.name
 # => #<struct Customer name="Joe Smith", address="123 Maple, Anytown NC", zip=12345>
 
-p joe.name       # => "Joe Smith"
-p joe['name']    # => "Joe Smith"
-p joe[:name]     # => "Joe Smith"
-p joe[0]         # => "Joe Smith"
-p joe.greeting   # => "Hello Joe Smith!"
-joe['name'] = 'John Smith'
-p joe[0]         # => "John Smith"
-joe.each {|x| p (x) }
-joe.each_pair {|name, value| p("#{name} => #{value}") }
-p joe.eql?(john)
-p joe.hash
-p john.hash
-p joe.length
-p joe.size
-p joe.members
-joe.select {|val| p val if val.is_a?(Fixnum)}
-p joe.to_a[1..2]
-p joe.values_at(1..2)
-p joe.values
-p joe.to_s
-p joe.inspect
+# p joe.name       # => "Joe Smith"
+# p joe['name']    # => "Joe Smith"
+# p joe[:name]     # => "Joe Smith"
+# p joe[0]         # => "Joe Smith"
+# p joe.greeting   # => "Hello Joe Smith!"
+# joe['name'] = 'John Smith'
+# p joe[0]         # => "John Smith"
+# joe.each {|x| p (x) }
+# joe.each_pair {|name, value| p("#{name} => #{value}") }
+# p joe.eql?(john)
+# p joe.hash
+# p john.hash
+# p joe.length
+# p joe.size
+# p joe.members
+# joe.select {|val| p val if val.is_a?(Fixnum)}
+# p joe.to_a[1..2]
+# p joe.values_at(1..2)
+# p joe.values
+# p joe.to_s
+# p joe.inspect
 
 
 # https://ru.wikibooks.org/wiki/Ruby/%D0%A1%D0%BF%D1%80%D0%B0%D0%B2%D0%BE%D1%87%D0%BD%D0%B8%D0%BA/Struct
