4ef9fe4f116b4cc937e27dba988e5ff5100ef423
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-10-21 14:49:00 +0300

    show result

--- init.rb
+++ init.rb
@@ -1,78 +1,62 @@
 require_relative('./Factory')
+require_relative('modules/terminalShow.rb')
+include TerminalShow
 
 Customer = Struct.new(:name, :address, :zip) do 
   def greeting
-    puts "Hello #{name}!"
+    "Hello #{name}!"
   end
 end
 
 FactoryClass = Factory.new(:name, :address, :zip) do 
   def greeting
-    puts "Hello #{name}!"
+    "Hello #{name}!"
   end
 end
 
 joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12345)
 joe2 = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12345)
 john = FactoryClass.new('John Smith', '123 Maple, Anytown NC', 12345)
-john2 = FactoryClass.new('John Smith', '123 Maple, Anytown NC', 12345)
-# p john.inspect
-# p john.name
-# p john['name']
-# p john[:name]
-# p john[0]
-# john[0] = 'Don Smith'
-# p john.name
-# john[:name] = 'Ron Smith'
-# p john.name
-# john['name'] = 'Joe Smith'
-# p john.name
-# p john.greeting
-# p john.values
-# p john.values[1..2]
-# john.each {|x| p "#{x.class}"}
-# john.each_pair{|name, value| p("#{name} => #{value}") }
-# p john.size
-# p john.length
-# p john.to_a[1..2]
-# p john.to_s
-# p john.inspect
-# p john.values_at(1..2)
-# p john.members
-# p john.eql?(john2)
-# p john == john2
-# p john.hash
-# p john2.hash
-john.select{|val| p val if val.is_a?(Fixnum)}
+john2 = FactoryClass.new('Smith John', '123 Maple, Anytown NC', 12345)
 
-
-# => #<struct Customer name="Joe Smith", address="123 Maple, Anytown NC", zip=12345>
-# p joe.name       # => "Joe Smith"
-# p joe['name']    # => "Joe Smith"
-# p joe[:name]     # => "Joe Smith"
-# p joe[0]         # => "Joe Smith"
-# p joe.greeting   # => "Hello Joe Smith!"
-# joe['name'] = 'John Smith'
-# p joe[0]         # => "John Smith"
-# joe.each {|x| p (x) }
-# joe.each_pair {|name, value| p("#{name} => #{value}") }
-# p joe.eql?(joe2)
-# p joe.hash
-# p joe2.hash
-# p john.hash
-# p joe.length
-# p joe.size
-# p joe.members
-joe.select {|val| p val if val.is_a?(Fixnum)}
-# p joe.to_a[1..2]
-# p joe.values_at(1..2)
-# p joe.values
-# p joe.to_s
-# p joe.inspect
-
-
-# https://ru.wikibooks.org/wiki/Ruby/%D0%A1%D0%BF%D1%80%D0%B0%D0%B2%D0%BE%D1%87%D0%BD%D0%B8%D0%BA/Struct
-# Методы класса
-# new
-# Методы объекта
+# instance methods:
 # []=, [], ==, each_pair, each, eql?, hash, inspect, length, members, select, size, to_a, to_s, values_at, values
+
+meth = [:name, :address, :zip, :greeting, :hash, :length, :members, :to_s, :values, :inspect]
+meth_with_val = {
+    each:       Proc.new {|x| p "#{x.class}"}, 
+    each_pair:  Proc.new {|name, value| p("#{name} => #{value}") },
+    select:     Proc.new {|val| p val if val.is_a?(Fixnum)},
+    values_at:  1,
+    eql?:       john2,
+}
+
+john2['name'] = 'Don Smith'
+john2['name']
+john2[:name] = 'Ron Smith'
+john2[0] = 'John Smith'
+
+putSeparator('Start',120)
+
+meth.each do |meth|
+    putSeparator("Joe(Struct) call method #{meth}",120)
+    p joe.send(meth)
+    putSeparator("John(Factory) call method #{meth}",120)
+    p john.send(meth)
+end
+
+meth_with_val.each do |meth,value|
+    if value.is_a?(Proc)
+        putSeparator("Joe(Struct) call method #{meth} with val #{value}",120)
+        p joe.send(meth,&value)
+        putSeparator("John(Factory) call method #{meth} with val #{value}",120)
+        p john.send(meth,&value)
+    else
+        putSeparator("Joe(Struct) call method #{meth} with val #{value}",120)
+        p joe.send(meth,value)
+        putSeparator("John(Factory) call method #{meth} with val #{value}",120)
+        p john.send(meth,value)
+    end
+end
+
+putSeparator('END',120)
