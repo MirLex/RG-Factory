100e8e1274567964014de9414dc6714552e86b69
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-10-25 20:09:46 +0300

    Code Cleaning

--- Factory.rb
+++ Factory.rb
@@ -1,75 +1,77 @@
+# frozen_string_literal: true
 class Factory
-    def self.new(*args, &block)
-        gen_class(*args,&block)
-    end
+  def self.new(*args, &block)
+    gen_class(*args, &block)
+  end
 
-    def self.gen_class(*args,&block)
-        Class.new  do
-            attr_accessor(*args)
-            
-            define_method :initialize do |*ins_args|
-                #TODO ArgumentError Wrong number of arguments (args.l !=  ins_args)
-                args.each_with_index do |arg,index|
-                    send("#{arg}=", ins_args[index])
-                end                
-            end
+  def self.gen_class(*args, &block)
+    Class.new do
+      attr_accessor(*args)
 
-            define_method :[] do |attr|
-                #TODO error if not Exist 
-                attr.is_a?(Fixnum) ? send("#{args[attr]}") : send(attr)
-            end
-
-            define_method :[]= do |attr , val|
-                attr.is_a?(Fixnum) ? send("#{args[attr]}=",val) : send("#{attr}=",val)
-            end
-
-            define_method :values do
-                args.map{|attr| send(attr)}
-            end
-
-            define_method :each do |&block|
-                values.each(&block)
-            end
-
-            define_method :each_pair do |&block|
-                hash_attr_val.each(&block)
-            end
-
-            define_method :length do
-                args.length
-            end
-
-            define_method :values_at do |index|
-                values[index]
-            end
-
-            define_method :members  do
-                args
-            end
-
-            define_method :hash do
-                hash_attr_val.hash
-            end
-
-            define_method :eql? do |other|
-                self.class == other.class ? hash == other.hash : false
-            end
-
-            define_method :select do |&block|
-                values.each(&block)
-            end
-            
-            alias :size :length
-            alias :to_a :values
-            alias :to_s :inspect
-            alias :==   :eql?
-
-            class_eval &block if block_given?
-            
-            private
-            define_method :hash_attr_val do
-                Hash[ args.map { |attr| [attr, send(attr)]} ]
-            end
+      define_method :initialize do |*ins_args|
+        # TODO: ArgumentError Wrong number of arguments (args.l !=  ins_args)
+        args.each_with_index do |arg, index|
+          send("#{arg}=", ins_args[index])
         end
+      end
+
+      define_method :[] do |attr|
+        # TODO: error if not Exist
+        attr.is_a?(Integer) ? send((args[attr]).to_s) : send(attr)
+      end
+
+      define_method :[]= do |attr, val|
+        attr.is_a?(Integer) ? send("#{args[attr]}=", val) : send("#{attr}=", val)
+      end
+
+      define_method :values do
+        args.map { |attr| send(attr) }
+      end
+
+      define_method :each do |&block|
+        values.each(&block)
+      end
+
+      define_method :each_pair do |&block|
+        hash_attr_val.each(&block)
+      end
+
+      define_method :length do
+        args.length
+      end
+
+      define_method :values_at do |index|
+        values[index]
+      end
+
+      define_method :members do
+        args
+      end
+
+      define_method :hash do
+        hash_attr_val.hash
+      end
+
+      define_method :eql? do |other|
+        self.class == other.class ? hash == other.hash : false
+      end
+
+      define_method :select do |&block|
+        values.each(&block)
+      end
+
+      alias_method :size, :length
+      alias_method :to_a, :values
+      alias_method :to_s, :inspect
+      alias_method :==, :eql?
+
+      class_eval &block if block_given?
+
+      private
+
+      define_method :hash_attr_val do
+        Hash[args.map { |attr| [attr, send(attr)] }]
+      end
     end
+  end
 end
--- init.rb
+++ init.rb
@@ -1,34 +1,35 @@
+# frozen_string_literal: true
 require_relative('./Factory')
 require_relative('modules/terminalShow.rb')
 include TerminalShow
 
-Customer = Struct.new(:name, :address, :zip) do 
+Customer = Struct.new(:name, :address, :zip) do
   def greeting
     "Hello #{name}!"
   end
 end
 
-FactoryClass = Factory.new(:name, :address, :zip) do 
+FactoryClass = Factory.new(:name, :address, :zip) do
   def greeting
     "Hello #{name}!"
   end
 end
 
-joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12345)
-joe2 = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12345)
-john = FactoryClass.new('John Smith', '123 Maple, Anytown NC', 12345)
-john2 = FactoryClass.new('Smith John', '123 Maple, Anytown NC', 12345)
+joe = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
+joe2 = Customer.new('Joe Smith', '123 Maple, Anytown NC', 12_345)
+john = FactoryClass.new('John Smith', '123 Maple, Anytown NC', 12_345)
+john2 = FactoryClass.new('Smith John', '123 Maple, Anytown NC', 12_345)
 
 # instance methods:
 # []=, [], ==, each_pair, each, eql?, hash, inspect, length, members, select, size, to_a, to_s, values_at, values
 
 meth = [:name, :address, :zip, :greeting, :hash, :length, :members, :to_s, :values, :inspect]
 meth_with_val = {
-    each:       Proc.new {|x| p "#{x.class}"}, 
-    each_pair:  Proc.new {|name, value| p("#{name} => #{value}") },
-    select:     Proc.new {|val| p val if val.is_a?(Fixnum)},
-    values_at:  1,
-    eql?:       john2,
+  each:       proc { |x| p x.class.to_s },
+  each_pair:  proc { |name, value| p("#{name} => #{value}") },
+  select:     proc { |val| p val if val.is_a?(Integer) },
+  values_at:  1,
+  eql?:       john2
 }
 
 john2['name'] = 'Don Smith'
@@ -36,27 +37,27 @@ john2['name']
 john2[:name] = 'Ron Smith'
 john2[0] = 'John Smith'
 
-putSeparator('Start',120)
+putSeparator('Start', 120)
 
 meth.each do |meth|
-    putSeparator("Joe(Struct) call method #{meth}",120)
-    p joe.send(meth)
-    putSeparator("John(Factory) call method #{meth}",120)
-    p john.send(meth)
+  putSeparator("Joe(Struct) call method #{meth}", 120)
+  p joe.send(meth)
+  putSeparator("John(Factory) call method #{meth}", 120)
+  p john.send(meth)
 end
 
-meth_with_val.each do |meth,value|
-    if value.is_a?(Proc)
-        putSeparator("Joe(Struct) call method #{meth} with val #{value}",120)
-        p joe.send(meth,&value)
-        putSeparator("John(Factory) call method #{meth} with val #{value}",120)
-        p john.send(meth,&value)
-    else
-        putSeparator("Joe(Struct) call method #{meth} with val #{value}",120)
-        p joe.send(meth,value)
-        putSeparator("John(Factory) call method #{meth} with val #{value}",120)
-        p john.send(meth,value)
-    end
+meth_with_val.each do |meth, value|
+  if value.is_a?(Proc)
+    putSeparator("Joe(Struct) call method #{meth} with val #{value}", 120)
+    p joe.send(meth, &value)
+    putSeparator("John(Factory) call method #{meth} with val #{value}", 120)
+    p john.send(meth, &value)
+  else
+    putSeparator("Joe(Struct) call method #{meth} with val #{value}", 120)
+    p joe.send(meth, value)
+    putSeparator("John(Factory) call method #{meth} with val #{value}", 120)
+    p john.send(meth, value)
+  end
 end
 
-putSeparator('END',120)
+putSeparator('END', 120)
